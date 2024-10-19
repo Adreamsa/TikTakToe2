9eadf6ab390d77afe90385a630ecae4eddc65533
Adrian <130883450+Adreamsa@users.noreply.github.com>
2024-10-18 18:14:22 -0600

    first commit

---
+++ src/Board.java
@@ -0,0 +1,15 @@
+public class Board {
+    int size;
+    void drawBoard (){
+
+    }
+    void initBoard (){
+
+    }
+    void updateBoard(){
+
+    }
+    void validateMove(int x, int y){
+
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,9 @@
+abstract class Game {
+    void nextTurn() {
+
+    }
+
+    boolean isGameOver() {
+        return false;
+    }
+}
